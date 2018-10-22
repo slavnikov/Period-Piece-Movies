8b147c7296c61237bd4498b763103c13dbea8672
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-10-22 18:43:04 -0400

    Bootstrap the current user to the window.

--- frontend/period_piece_movies.jsx
+++ frontend/period_piece_movies.jsx
@@ -6,16 +6,27 @@ import configureStore from './store/store';
 import { fetchMovie } from './actions/movie_actions';
 import { fetchLocation } from './api/external_requests';
 
-const store = configureStore();
-
-window.getState = store.getState;
-window.dispatch = store.dispatch;
-
-window.fetchLocation = fetchLocation;
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
+
+  let preLoadedState = {};
+
+  if (window.currentUser) {
+    preLoadedState = {
+      session: {
+        currentUserId: window.currentUser.id
+      },
+      users: {
+        [window.currentUser.id]: window.currentUser,
+      }
+    };
+    delete(window.currentUser);
+  }
+
+  const store = configureStore(preLoadedState);
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   ReactDOM.render(<Linker store={store}/>, root);
 });
