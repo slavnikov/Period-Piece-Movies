1fd8119a860cb03b18f932461c602105e7db09ec
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-10-22 17:49:50 -0400

    Add differential rendering of header based on logged in status.

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -6,13 +6,28 @@ class Header extends React.Component {
     super(props);
   }
 
+  variableContent() {
+    if (this.props.currentUser) {
+      return (
+        <div className='glass-container flex-column-center' id='inner-header'>
+          <h5 className='pseudo-center'>{this.props.currentUser.email}</h5>
+          <form><button>logout</button></form>
+        </div>
+      );
+    } else {
+      return (
+        <div className='glass-container flex-column-center' id='inner-header'>
+          <h5 className='pseudo-center'>HOVER TO <button>SIGN IN</button> OR <button>REGISTER</button></h5>
+          <Register createUser={this.props.createUser}/>
+        </div>
+      );
+    }
+  }
+
   render () {
     return (
       <div className='glass-pane' id='header'>
-        <div className='glass-container flex-column-center' id='inner-header'>
-          <h5 className='pseudo-center'>HOVER TO SIGN IN OR REGISTER</h5>
-          <Register createUser={this.props.createUser}/>
-        </div>
+        {this.variableContent()}
       </div>
     );
   }
--- frontend/components/header/header_container.js
+++ frontend/components/header/header_container.js
@@ -2,10 +2,16 @@ import { connect } from 'react-redux';
 import Header from './header';
 import { createUser } from '../../actions/user_actions';
 
+const mapStateToProps = (state) => {
+  return ({
+    currentUser: state.users[state.session.currentUserId],
+  });
+};
+
 const mapDispatchToProps = (dispatch) => {
   return ({
     createUser: (params) => dispatch(createUser(params)),
   });
 };
 
-export default connect(null, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
